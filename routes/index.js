92a608bce2d10b9309c74f46bfd6b289aeab9d40
Mauricio Palomares <mau.palo2@gmail.com>
2022-09-21 15:11:12 -0500

    Promedio

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,37 @@ passport.deserializeUser(
 });
 });
 
+async function detalleUsu(id){
+  await client.connect();
+      const db = client.db(dbName);
+      const collection = db.collection('Grades');
+      let arregloMat = await collection.aggregate([{$match:{student_id:id}}]).toArray();
+      let promedio = await collection.aggregate(
+        [
+          {
+            '$match': {
+              'student_id': id
+            }
+          }, {
+            '$project': {
+              '_id': 0, 
+              'student_id': 1, 
+              'promedio': 1
+            }
+          }, {
+            '$group': {
+              '_id': '$student_id', 
+              'x': {
+                '$avg': '$promedio'
+              }
+            }
+          }
+        ]
+      ).toArray();
+      var dato = {arregloMat, promedio}
+      console.log(dato)
+      return dato;
+  };
 
 /* GET home page. */
 router.get('/',(req, res, next) => {
@@ -23,7 +54,18 @@ router.get('/',(req, res, next) => {
   }
 }, function(req, res, next) {
 
-          res.render('index', { title: "Menú Principal", student_id:req.user.student_id});
+          //res.render('index', { title: "Menú Principal", student_id:req.user.student_id});
+          detalleUsu(req.user.student_id)
+          .then((dato)=>{
+            console.log(dato.promedio)
+            res.render('index', { title: "Menú Principal", student_id:req.user.student_id, materias:dato.arregloMat, promedio: dato.promedio[0].x});
+          })  
+          .catch((err)=>{
+              console.log(err);
+          })
+          .finally(()=>{
+              client.close
+          })
 
   
 });
