92d0b999015d8313c6556ae1b13813ae72d833ce
Mauricio Palomares <mau.palo2@gmail.com>
2022-09-23 15:41:17 -0500

    Añadir fechas

--- routes/index.js
+++ routes/index.js
@@ -40,6 +40,33 @@ async function detalleUsu(id){
           }
         ]
       ).toArray();
+      let fechas = await db.collection('usuarios').aggregate(
+        [
+          {
+            '$match': {
+              'student_id': 10
+            }
+          }, {
+            '$project': {
+              '_id': 0, 
+              'student_id': 1, 
+              'inicio': {
+                '$dateToString': {
+                  'format': '%d-%m-%Y', 
+                  'date': '$inicio_beca'
+                }
+              }, 
+              'fin': {
+                '$dateToString': {
+                  'format': '%d-%m-%Y', 
+                  'date': '$fin_beca'
+                }
+              }
+            }
+          }
+        ]
+      ).toArray()
+      console.log(fechas)
       let beca =0;
       if (promedio >=70){
         beca = 10
@@ -53,7 +80,7 @@ async function detalleUsu(id){
       if(promedio == 100){
         beca = 40;
       }
-      var dato = {arregloMat, promedio, beca}
+      var dato = {arregloMat, promedio, beca, fechas}
       console.log(dato)
       return dato;
   };
@@ -71,7 +98,7 @@ router.get('/',(req, res, next) => {
           detalleUsu(req.user.student_id)
           .then((dato)=>{
             console.log(dato.promedio)
-            res.render('index', { title: "Menú Principal", student_id:req.user.student_id, materias:dato.arregloMat, promedio: dato.promedio[0].x, beca:dato.beca});
+            res.render('index', { title: "Menú Principal", student_id:req.user.student_id, materias:dato.arregloMat, promedio: dato.promedio[0].x, beca:dato.beca, fechas:dato.fechas[0]});
           })  
           .catch((err)=>{
               console.log(err);
